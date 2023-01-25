cfca367ea612148f5ac7aa8f76428a1875875176
Matthias Luebken <matthias.luebken@gmail.com>
2023-01-25 18:28:56 +0100

    css
    Signed-off-by: Matthias Luebken <matthias.luebken@gmail.com>

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"embed"
 	"html/template"
+	"net/http"
 
 	"github.com/luebken/helloweb/pkg/web"
 
@@ -11,12 +12,16 @@ import (
 
 //go:embed templates/*
 var resources embed.FS
-
 var templ = template.Must(template.ParseFS(resources, "templates/*"))
+
+//go:embed static/*
+var staticFiles embed.FS
+var staticFS = http.FS(staticFiles)
 
 func main() {
 	r := gin.Default()
 	r.SetHTMLTemplate(templ)
+	r.StaticFS("/static/", staticFS)
 	r.GET("/", web.IndexHandler())
 	r.Run() // listen and serve on 0.0.0.0:8080
 }
